5a4e1cd2dfd1679c53f5c39140b11bb3bb4befcc
Luke <luke@lukecreates.com>
2013-06-12 16:28:51 -0400

    Adds support for multiple formats (PNG default, jpeg fallback) and refactors code.

--- product-pngs-script.js
+++ product-pngs-script.js
@@ -8,102 +8,115 @@
 runthis();
 function runthis(){
     var path 			= Folder.selectDialog ("Please choose the location of the source image files.", Folder.myDocuments);
-    var inputFolder 	= new Folder(path);
-    var inputFiles 		= inputFolder.getFiles(/\.(jpg|jpeg|)$/i);
-    var processedPath	= "formated";
-    var retina_sufix	= "-2x";
-    var thumb_sufix		= "_thumb";
-    var mini_sufix		= "_mini";
+    	inputFolder 	= new Folder(path);
+    	inputFiles 		= inputFolder.getFiles(/\.(jpg|jpeg|)$/i);
+    	processedPath	= "formated";
+    	retina_sufix	= "-2x";
+    	thumb_sufix		= "_thumb";
+    	mini_sufix		= "_mini";
+    	exportPNG		= true;
+    	imgPad 			= 0;
 
-    // png values
-	var finalWidth		= 600;
-	// 1st thumb
-	var thumbHeight		= 300;
-	var thumbWidth		= 300;
-	// mini thumbs
-	var miniHeight	= 120;
-	var miniWidth	= 130;
+    // export quality
+	var jpegQuality		= 80;
+		pngQuality		= 80;
 
-	var pngPad 			= 20;
+    // size values
+	var fullSize		= 500;
+		thumbHeight		= 350;
+		thumbWidth		= 350;
+		miniHeight		= 120;
+		miniWidth		= 130;
 
+	// PNG export options
+	var pngOpts, file;
+        pngOpts = new ExportOptionsSaveForWeb();
+        pngOpts.format = SaveDocumentType.PNG;
+        pngOpts.PNG8 = false;
+        pngOpts.quality = pngQuality;
+        interlaced = true;
+
+	// JPG export options
+    var sfwOpts = new ExportOptionsSaveForWeb(); 
+		sfwOpts.format = SaveDocumentType.JPEG; 
+		sfwOpts.includeProfile = false; 
+		sfwOpts.interlaced = 0; 
+		sfwOpts.optimized = true; 
+		sfwOpts.quality = jpegQuality; //0-100
+
+
+	// Choose export options
 	var retina = confirm("Retina Versions?");
-
-	var createPNG = confirm("Create Full Size Images?");
-
-	var createThumbOne = confirm("Create Thumbnails " + thumbHeight + "x" + thumbWidth + "?");
+		createFull = confirm(fullSize +"px max height images?");
+		createThumbs = confirm("Thumbnails?");
 	
-	var createThumbTwo = confirm("Create mini Thumbnails " + miniHeight + "x" + miniWidth + "?");
-	
+	if (createThumbs == true) {
+		var createThumbOne = confirm("Create Thumbnails " + thumbHeight + "x" + thumbWidth + "?");
+		var createThumbTwo = confirm("Create mini Thumbnails " + miniHeight + "x" + miniWidth + "?");
+	}
+		
 // =======================================================================// 
 //                    		 PNG & Image Resizing			              //        
 // =======================================================================//  
 
 		for(index in inputFiles){	     
-			//open file
-		    open(new File(inputFiles[index]));
 
+		    open(new File(inputFiles[index]));
 			var doc = app.activeDocument;
 			
-			var pngOpts, file;
-		        pngOpts = new ExportOptionsSaveForWeb();
-		        pngOpts.format = SaveDocumentType.PNG;
-		        pngOpts.PNG8 = false;
-		        pngOpts.quality = 100;
-		        interlaced = true;
-
-
 			if(app.documents.length > 0) {
 			var n = doc.pathItems.length;
 
 				if((n>0) && (doc.pathItems[0].name!="Work path")){
-					
 					var layerRef = doc.layers[i];
-
-					if (doc.activeLayer.isBackgroundLayer == true) {
-						doc.activeLayer.isBackgroundLayer = false;
-						doc.activeLayer.name  = 'Layer0';
-					}
 
 		// ************************** Functions ***************************//
 
 					// working
 					function imageSize(size, percent, padding){
-						padding = (typeof padding === "undefined") ? pngPad : padding;
+						padding = (typeof padding === "undefined") ? imgPad : padding;
 						percent = (typeof percent === "undefined") ? 1 : percent;
 						return (size*percent) - (padding*2);
 					};
 
 					function canvasSize(size, percent, padding){
-						padding = (typeof padding === "undefined") ? pngPad : padding;
+						padding = (typeof padding === "undefined") ? imgPad : padding;
 						percent = (typeof percent === "undefined") ? 1 : percent;
 						return (size*percent) + (padding*2);
 					};
 
-					function exportPNG(suffix, folder){
+					function exportFileType(suffix, folder){
 						folder = (typeof folder === "undefined") ? processedPath : folder;
 						suffix = (typeof suffix === "undefined") ? "" : suffix;
+
 						var docName = doc.name;
 						docName = docName.match(/(.*)(\.[^\.]+)/) ? docName = docName.match(/(.*)(\.[^\.]+)/):docName = [docName, docName];
 						var finalFolderPath = (decodeURI(doc.path)+'/' + folder);
-						var saveName = new File(finalFolderPath + '/' + docName[1] + suffix + '.png');
-						if(!finalFolderPath.exists) new Folder(finalFolderPath).create();
-						doc.exportDocument(saveName,ExportType.SAVEFORWEB,pngOpts);
+
+						if (exportPNG == true){	var typeOpts = pngOpts; var fileExt = '.png'; }
+						else{ var typeOpts = sfwOpts; var fileExt = '.jpg' }
+
+						var saveName = new File(finalFolderPath + '/' + docName[1] + suffix + fileExt);
+							if(!finalFolderPath.exists) new Folder(finalFolderPath).create();
+							doc.exportDocument(saveName,ExportType.SAVEFORWEB,typeOpts);
 						};
+					}
+
 					
 					function resize(targetWidth,percent){
 						percent = (typeof percent === "undefined") ? 1 : percent;
 						var targetWidth = imageSize(targetWidth, percent);
-						var docRatio = (doc.height / doc.width);
+							docRatio = (doc.height / doc.width);
 			
-						if(docRatio <= 2) doc.resizeImage(width=targetWidth);
-						else doc.resizeImage(height=targetWidth);
+						if(docRatio <= 2) doc.resizeImage(null,height=targetWidth);
+						else doc.resizeImage(null,height=targetWidth);
 						doc.resizeCanvas(width=canvasSize(doc.width),height=canvasSize(doc.height),null,ResampleMethod.BICUBIC);
 					};
 					
 					function resizeThumb(targetWidth, targetHeight, percent){
 						percent = (typeof percent === "undefined") ? 1 : percent;
 						var ratio =(targetHeight / targetWidth);
-						var docRatio =(doc.height / doc.width);
+							docRatio =(doc.height / doc.width);
 
 					    if(docRatio >= ratio){
 					    	doc.resizeImage(null,height=targetHeight*percent,null,ResampleMethod.BICUBIC);
@@ -115,30 +128,46 @@ function runthis(){
 						};
 					}
 
-		// ************************** Trim to Path **************************//
+		// ************************** Setup **************************//
+
 
 					doc.changeMode.RGB;
-					doc.pathItems[0].makeSelection();
-					doc.selection.invert();
-					doc.selection.clear();
-					doc.trim(TrimType.TRANSPARENT);
 
-		// ************************** PNG Creation ***************************//
+					if (doc.activeLayer.isBackgroundLayer == true) {
+						doc.activeLayer.isBackgroundLayer = false;
+						doc.activeLayer.name  = 'Layer0';
+					}
+
+		// ************************** Trim to Path **************************//
+
+					// if (exportPNG == true) {
+						if (doc.pathItems.length != 0){
+							doc.pathItems[0].makeSelection();
+							doc.selection.invert();
+							doc.selection.clear();
+							doc.trim(TrimType.TRANSPARENT);
+						}
+						else {
+							exportPNG = false
+						}
+					// }
+
+		// ************************** Full Size Creation ***************************//
 
 					// resize 
-					if (createPNG == true){ 
+					if (createFull == true){ 
 						if (retina == true){ 
-							resize(finalWidth,2);
-							exportPNG(retina_sufix);
-							resize(finalWidth);
-							exportPNG();
+							resize(fullSize,2);
+							exportFileType(retina_sufix);
+							resize(fullSize);
+							exportFileType();
 						} 
 						else { 
-							resize(finalWidth);
-							exportPNG();
+							resize(fullSize);
+							exportFileType();
 						}
 					}
-					
+
 		// ******************************* Thumbnail Creation ***************************//
 
 
@@ -146,27 +175,28 @@ function runthis(){
 						if (retina == true){
 						var inc_retina = thumb_sufix + retina_sufix;
 						resizeThumb(thumbWidth, thumbHeight, 2);
-					    exportPNG(inc_retina);
+					    exportFileType(inc_retina);
 					    resizeThumb(thumbWidth, thumbHeight);
-					    exportPNG(thumb_sufix);
+					    exportFileType(thumb_sufix);
 						} 
 						else {
 							resizeThumb(thumbWidth, thumbHeight);
-						    exportPNG(thumb_sufix);
+						    exportFileType(thumb_sufix);
 						}
 					}
+
 					// resize n.2
 					if (createThumbTwo == true){
 						if (retina == true){
 						var inc_retina = mini_sufix + retina_sufix;
 						resizeThumb(miniWidth, miniHeight, 2);
-					    exportPNG(inc_retina);
+					    exportFileType(inc_retina);
 					    resizeThumb(miniWidth, miniHeight);
-					    exportPNG(mini_sufix);
+					    exportFileType(mini_sufix);
 						} 
 						else {
 							resizeThumb(miniWidth, miniHeight);
-						    exportPNG(mini_sufix);
+						    exportFileType(mini_sufix);
 						}
 					}
 					
@@ -174,5 +204,3 @@ function runthis(){
 				}
 			}
 		}
-	}
-
